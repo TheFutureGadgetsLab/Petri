5da57ccd2a541ec66f363813772cb2e72405a8ec
Haydn Jones <haydnjonest@gmail.com>
2021-06-15 22:08:32 -0600

    Springs

--- Assets/JeffDoBeGoing.cs
+++ Assets/JeffDoBeGoing.cs
@@ -8,7 +8,7 @@ public class JeffDoBeGoing : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        var force = new Vector2(Random.Range(-100f, 100f), Random.Range(-100f, 100f));
+        var force = new Vector2(Random.Range(-500f, 500f), Random.Range(-500f, 500f));
 
         var rigbod = GetComponent<Rigidbody2D>();
         rigbod.AddForce(force);
@@ -17,6 +17,15 @@ public class JeffDoBeGoing : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        var line = GetComponent<LineRenderer>();
+        var spring = GetComponent<SpringJoint2D>();
+        if (line == null) {
+            return;
+        }
+        var body = spring.attachedRigidbody;
+
+        line.SetPosition(0, spring.connectedBody.position);
+        line.SetPosition(1, GetComponent<Transform>().position);
     }
     
 }
