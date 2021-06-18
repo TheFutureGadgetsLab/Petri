38c9e1ca6b37382df3194f902bbbd0f0c6a3f19d
Haydn Jones <haydnjonest@gmail.com>
2021-06-18 09:39:21 -0600

    ConstantForce2D in propulsion

--- Assets/Scripts/Propulsion.cs
+++ Assets/Scripts/Propulsion.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class Propulsion : JeffDoBeGoing
 {
-    float force;
+    public Vector2 minMaxForce = new Vector2(0.0f, 10.0f);
+    public Vector2 minMaxTorque = new Vector2(-7.0f, 7.0f);
 
     new protected void Start() {
         base.Start();
-        force = Random.Range(0.5f, 10.0f);
-        transform.Rotate(Vector3.forward * Random.Range(-180, 180));
-    }
+        Debug.Log("Setting force");
 
-    private void FixedUpdate() {
-        if (rigidbody.velocity.magnitude < force) {
-            rigidbody.AddRelativeForce(new Vector2(force - rigidbody.velocity.magnitude, 0.0f));
-        }
+        // Set a random rotation
+        transform.Rotate(Vector3.forward * Random.Range(-180, 180));
+
+        var constantForce = GetComponent<ConstantForce2D>();
+        constantForce.relativeForce = new Vector2(Random.Range(minMaxForce.x, minMaxForce.y), Random.Range(minMaxForce.x, minMaxForce.y));
+
+        constantForce.torque = Random.Range(minMaxTorque.x, minMaxTorque.y);
     }
 }
